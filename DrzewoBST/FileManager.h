cbafbc5d951d257801dba30b40dcb6756865075b
Ja <adrian.potoczak@wp.pl>
2025-11-09 16:34:08 +0100

    Dodanie szkieletu i implementacji w FileManager.h

--- DrzewoBST/FileManager.h
+++ DrzewoBST/FileManager.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <string>
+#include "BST.h"
+
+class FileManager {
+public:
+    static bool saveBinary(const BST& tree, const std::string& filename);
+
+    static bool loadBinary(BST& tree, const std::string& filename);
+
+    static bool loadTextToBST(BST& tree, const std::string& filename, bool append = false);
+};
